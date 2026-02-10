1c841f2ffcd5c08509d0df628f6c87bf061f49a7
ShD8686 <241718@astanait.edu.kz>
2026-02-11 04:57:50 +0500

    Fix formatting and remove unnecessary comments in Game.js

--- models/Game.js
+++ models/Game.js
@@ -4,9 +4,10 @@ const gameSchema = new mongoose.Schema({
     title: { type: String, required: true },
     genres: [{ type: String }],
     releaseYear: { type: Number, required: true },
-    price: { type: Number, default: 0 }, // НОВОЕ: Цена в $
-    description: { type: String },       // НОВОЕ: Описание игры
+    price: { type: Number, default: 0 },
+    description: { type: String },
     developer: { type: mongoose.Schema.Types.ObjectId, ref: 'Developer', required: true }
 }, { timestamps: true });
+
 
 module.exports = mongoose.model('Game', gameSchema);
